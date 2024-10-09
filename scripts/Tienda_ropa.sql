879c753fe5861b9acb0cb8e823851bcce4633a08
Keyner-Mena <keynermena896@gmail.com>
2024-10-08 20:00:52 -0600

    INSERT BD

--- scripts/Tienda_ropa.sql
+++ scripts/Tienda_ropa.sql
@@ -44,3 +44,39 @@ CREATE TABLE Detalle_Venta (
     FOREIGN KEY (ID_venta) REFERENCES Ventas(ID_venta)
 );
 
+USE TiendaRopa;
+-- Insertar datos en la tabla Marca
+INSERT INTO Marca (Nombre, Descripción, País_de_origen, Categoría, Contacto)
+VALUES
+('Nike', 'Marca de ropa deportiva y accesorios', 'EE.UU.', 'Deportiva', 'contact@nike.com'),
+('Adidas', 'Marca de calzado y ropa deportiva', 'Alemania', 'Deportiva', 'info@adidas.com'),
+('Zara', 'Ropa de moda y accesorios', 'España', 'Casual', 'zara@zara.com'),
+('H&M', 'Ropa de moda a precios asequibles', 'Suecia', 'Casual', 'support@hm.com'),
+('Levis', 'Ropa de mezclilla y jeans', 'EE.UU.', 'Casual', 'levis@levis.com');
+
+-- Insertar datos en la tabla Prenda
+INSERT INTO Prenda (ID_Marca, Tipo_de_prenda, Material, Talla, Color, Precio, Stock)
+VALUES
+(1, 'Camiseta', 'Algodón', 'M', 'Blanco', 19.99, 50),
+(2, 'Zapatillas', 'Sintético', '42', 'Negro', 59.99, 30),
+(3, 'Chaqueta', 'Poliéster', 'L', 'Azul', 79.99, 20),
+(4, 'Vestido', 'Seda', 'S', 'Rojo', 49.99, 15),
+(5, 'Jeans', 'Denim', '32', 'Azul', 39.99, 25);
+
+-- Insertar datos en la tabla Ventas
+INSERT INTO Ventas (Fecha_venta, Metodo_pago, Total_venta)
+VALUES
+('2024-10-05 10:30:00', 'Tarjeta de crédito', 39.98),
+('2024-10-06 14:15:00', 'Efectivo', 59.99),
+('2024-10-07 11:00:00', 'PayPal', 79.99),
+('2024-10-08 09:45:00', 'Tarjeta de débito', 49.99),
+('2024-10-08 16:20:00', 'Transferencia bancaria', 119.97);
+
+-- Insertar datos en la tabla Detalle_Venta
+INSERT INTO Detalle_Venta (ID_prenda, ID_venta, Cantidad, Subtotal)
+VALUES
+(1, 1, 2, 39.98),  -- 2 Camisetas en la venta 1
+(2, 2, 1, 59.99),  -- 1 Par de zapatos en la venta 2
+(3, 3, 1, 79.99),  -- 1 Chaqueta en la venta 3
+(4, 4, 1, 49.99),  -- 1 Vestido en la venta 3
+(5, 5, 3, 119.97); -- 3 Jeans en la venta 5
